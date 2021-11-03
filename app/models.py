8f9edffa06edefc4decc57e3751784a469e81c1f
othienoJoe <bryo094@gmail.com>
2021-11-03 23:28:07 +0300

    installed cloudinary and updated the views and models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Company(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=250)
+	description = models.TextField()
+	image = CloudinaryField("image")
+	url = models.URLField(blank=True)
+	location = models.CharField(max_length=100, default="Nairobi")
+	# usability_rate = models.IntegerField(default=0, blank=True, null=True)
+	# content_rate = models.IntegerField(default=0, blank=True, null=True)
+	date = models.DateTimeField(auto_now_add=True, null=True)
+
+	@classmethod
+	def search_by_title(cls, search_term):
+			projects = cls.objects.filter(title__icontains=search_term)
+			return projects
+
+	@classmethod
+	def get_project_by_id(cls, id):
+			project = cls.objects.get(id=id)
+			return project
+
+	@classmethod
+	def get_all_projects(cls):
+			projects = cls.objects.all()
+			return projects
+
+	@classmethod
+	def get_all_projects_by_user(cls, user):
+			projects = cls.objects.filter(user=user)
+			return projects
+
+	# update project
+	def update_project(self, **kwargs):
+			for key, value in kwargs.items():
+					setattr(self, key, value)
+			self.save()
+
+	def save_project(self):
+			self.save()
+
+	def delete_project(self):
+			self.delete()
+
+	def __str__(self):
+			return self.title
+
+
+# profile models
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField("image")
+    bio = models.TextField(max_length=250, blank=True, null=True)
+    contact = models.CharField(max_length=250, blank=True, null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+    def __str__(self):
+        return self.user.username
+
+
+# rating models
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    design_rate = models.IntegerField(default=0, blank=True, null=True)
+    usability_rate = models.IntegerField(default=0, blank=True, null=True)
+    content_rate = models.IntegerField(default=0, blank=True, null=True)
+    avg_rate = models.IntegerField(default=0, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def save_rating(self):
+        self.save()
+
+    def delete_rating(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        rating = Rating.objects.filter(id=id).first()
+        return rating
+
+    def __str__(self):
+        return self.user.username
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+
 
 # Create your views here.
+def index(request):  # Home page
+	company = Company.objects.all()
+	# get the latest company from the database
+	latest_company = company[0]
+	# get company rating
+	rating = Rating.objects.filter(project_id=latest_company.id).first()
+
+	return render(
+		request, "index.html", {"companies": company, "project_home": latest_company, "rating": rating}
+  )
+
+# single company page
+def Company_details(request, project_id):
+	company = Company.objects.get(id=project_id)
+	# get company rating
+	rating = Rating.objects.filter(company = company)
+	return render(request, "company.html", {"project": company, "rating": rating})
